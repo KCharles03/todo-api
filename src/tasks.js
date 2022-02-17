5898c347ade258339febf781a11a508f4fec4fb0
“Keren <kerencharles4@gmail.com>
2022-02-17 10:57:36 -0500

    todo-api-db

---
+++ src/tasks.js
@@ -0,0 +1,12 @@
+
+
+// exports.createTask = (request, response) => {
+
+// }
+
+exports.getTasks = (request, response) => {
+    response.send('Get Tasks is working')
+}
+// exports.updateTasks = (request, response) => {
+
+// }
